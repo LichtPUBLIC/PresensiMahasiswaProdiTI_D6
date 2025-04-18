3e8f09596b7029a4296be22d552747adca98a8e6
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-04-18 12:57:04 +0700

    Add project files.

---
+++ PABD/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PABD
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Server=MSI\\DAFFAALYANDRA;Database=PresensiMahasiswaProdiTI;User Id=SA;Password=isiPasswordKamu;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Foem1_load(object sennder, EventArgs e)
+        {
+
+        }
+    }
+}
+
+           
